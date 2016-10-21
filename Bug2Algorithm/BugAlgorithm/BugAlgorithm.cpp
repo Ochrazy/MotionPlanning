00f9ec969dbd1b0157056f6f32fa31c83b4da090
Johannes Wambach <johannes.wambach@gmail.com>
2016-10-21 17:53:36 +0200

    Add files via upload

--- Bug2Algorithm/BugAlgorithm/BugAlgorithm.cpp
+++ Bug2Algorithm/BugAlgorithm/BugAlgorithm.cpp
@@ -3,14 +3,13 @@
 using namespace std;
 
 BugAlgorithm::BugAlgorithm(const string& name)
-	: firstMove(true)
-	, totalDistance(0.0)
-	, goalPosition(1.0, 1.0, 0.0) //set the goal position
+	: goalPosition(1.0, 1.0, 0.0) //set the goal position
 	, startPosition(0.0, 0.0, 0.0) //set the start position
-	, firstHit(false)
 	, wallFollowingMode(false)
 	, bWallFollowingClockwise(false)
-	, cornerPoint(Point(-99, 0, 0))
+	, cornerPoint(Point(nanf(" "), 0, 0))
+	, dist_current(DIST_MIN)
+	, obstacleHit(-1)
 {
 }
 
@@ -81,18 +80,6 @@ double BugAlgorithm::distanceEuclid(Point positionA, Point positionB)
 	return sqrt((x_diff * x_diff) + (y_diff * y_diff));// + (z_diff * z_diff));
 }
 
-/**********************************************************************************************/
-void BugAlgorithm::setTotalDistance(double value)
-{
-	totalDistance = value;
-}
-
-/**********************************************************************************************/
-double BugAlgorithm::getTotalDistance()
-{
-	return totalDistance;
-}
-
 /*************************************************************************************************************************/
 bool BugAlgorithm::update(Box obstacle[], Box robot[], int nObst)
 {
@@ -100,13 +87,14 @@ bool BugAlgorithm::update(Box obstacle[], Box robot[], int nObst)
 	static Box mink_diff[2] = { obstacle[0].MinkowskiDifference(robot[0]),
 								obstacle[1].MinkowskiDifference(robot[0]) };
 	static int ret = -1;  // no obstacle
-	double dist_current = DIST_MIN;
+	dist_current = DIST_MIN;
 
 	if (goalReached(robotPos, goalPosition, dist_current)) {
 		actPoint = goalPosition;
 		// cout << "at goal, smile :) \n";
 		return true;
 	}
+
 	// First Phase: Move to Goal
 	if (wallFollowingMode == false)
 	{
@@ -114,9 +102,9 @@ bool BugAlgorithm::update(Box obstacle[], Box robot[], int nObst)
 		heading = (goalPosition - robotPos).Normalize();
 
 		// Check if robo could move to goal without collision
+		obstacleHit = -1;
 		Point possibleRobotPos = robotPos + heading * dist_current;
-		int obstacleHit = -1;
-		if ((obstacleHit = obstacleInWay(mink_diff, robot, possibleRobotPos, nObst)) == -1)
+		if ((obstacleHit = obstaclesInWay(mink_diff, possibleRobotPos, nObst)) == -1)
 		{
 			// Free to move to goal
 		}
@@ -128,56 +116,8 @@ bool BugAlgorithm::update(Box obstacle[], Box robot[], int nObst)
 			// Follow the wall
 			wallFollowingMode = true;
 
-			// Move along wall clockwise or counterclockwise?
-			double direction = 1;
-			if (bWallFollowingClockwise == false)
-				direction = -1;
-
-			// Find Heading along Wall
-			Box boxHit = mink_diff[obstacleHit];
-			// Check on what side of the rectangle/obstacle robo is and set heading accordingly
-			// Left side
-			if (robotPos.x < boxHit.GetVertex(0).x && robotPos.x < boxHit.GetVertex(3).x)
-			{
-				// Bottom-Left
-				if (robotPos.y < boxHit.GetVertex(0).y && robotPos.y < boxHit.GetVertex(1).y)
-				{
-					if (bWallFollowingClockwise) heading = Point(0, direction, 0);
-					else heading = Point(-direction, 0, 0);
-				}
-				// Top-Left
-				else if (robotPos.y > boxHit.GetVertex(3).y && robotPos.y > boxHit.GetVertex(2).y)
-				{
-					if (bWallFollowingClockwise) heading = Point(direction, 0, 0);
-					else heading = Point(0, direction, 0);
-				}
-				else // Only Left side
-					heading = Point(0, direction, 0);
-			}
-			// Right side
-			else if (robotPos.x > boxHit.GetVertex(1).x && robotPos.x > boxHit.GetVertex(2).x)
-			{
-				// Bottom-Right
-				if (robotPos.y < boxHit.GetVertex(0).y && robotPos.y < boxHit.GetVertex(1).y)
-				{
-					if (bWallFollowingClockwise) heading = Point(-direction, 0, 0);
-					else heading = Point(0, -direction, 0);
-				}
-				// Top-Right
-				else if (robotPos.y > boxHit.GetVertex(3).y && robotPos.y > boxHit.GetVertex(2).y)
-				{
-					if (bWallFollowingClockwise) heading = Point(0, -direction, 0);
-					else heading = Point(direction, 0, 0);
-				}
-				else // Only Right side
-					heading = Point(0, -direction, 0);
-			}
-			// Only Top side
-			else if (robotPos.y > boxHit.GetVertex(3).y && robotPos.y > boxHit.GetVertex(2).y)
-				heading = Point(direction, 0, 0);
-			// Only Bottom side
-			else if (robotPos.y < boxHit.GetVertex(0).y && robotPos.y < boxHit.GetVertex(1).y)
-				heading = Point(-direction, 0, 0);
+			// Find Heading along wall
+			findHeadingAlongWall(robotPos, mink_diff[obstacleHit]);
 		}
 	}
 	else // Second Phase: Wall Following Mode
@@ -189,46 +129,16 @@ bool BugAlgorithm::update(Box obstacle[], Box robot[], int nObst)
 			return true;
 		}
 
-		// Turn 90 degrees to left/right 
-		Point heading90 = Point(heading.y, -heading.x, 0);
-		if (bWallFollowingClockwise == false)
-			heading90 = Point(-heading.y, heading.x, 0);
-
-		// Check if the obstacle/wall is still to right/left
-		Point possibleRobotPos = robotPos + heading90 * dist_current;
-		if (obstacleInWay(mink_diff, robot, possibleRobotPos, nObst) == -1)
-		{
-			// Free to move on = corner
-			heading = heading90;
-			cornerPoint = robotPos;
-		}
-
-		if (cornerPoint.x > -98)
-		{
-			// Robo hit m-line? 
-			Point inter;
-			double t1, t2;
-			Point possibleRobotPos3 = robotPos + heading * dist_current;
-			float p1x = robotPos.x, p1y = robotPos.y, p2x = possibleRobotPos3.x, p2y = possibleRobotPos3.y,
-				p3x = startPosition.x, p3y = startPosition.y, p4x = goalPosition.x, p4y = goalPosition.y;
-			if (IntersectionLineLine(cornerPoint, Point(p2x, p2y, 0), Point(p3x, p3y, 0), Point(p4x, p4y, 0),
-				&inter, &t1, &t2))
-			{
-				// Move to intersection
-				dist_current = robotPos.Distance(inter);
-				wallFollowingMode = false;
-				std::cout << "Leave Point: " << robotPos.x << "," << robotPos.y << std::endl;
-				cornerPoint.x = -99;
-			}
-		}
+		wallFollowing(robotPos, mink_diff[obstacleHit]);
 	}
 
+	// Move with current heading and dist_current
 	actPoint.Mac(heading, dist_current); // one step forward
 	return false;
 }
 
 /*****************************************************************************/
-int BugAlgorithm::obstacleInWay(Box obstacle[], Box robot[], Point robotPos, int nObst)
+int BugAlgorithm::obstaclesInWay(Box obstacle[], Point robotPos, int nObst)
 {
 	for (int i = 0; i < nObst; i++)
 	{
@@ -241,131 +151,15 @@ int BugAlgorithm::obstacleInWay(Box obstacle[], Box robot[], Point robotPos, int
 	return(-1);
 }
 
-/******************************************************************************/
-void BugAlgorithm::move()
+bool BugAlgorithm::obstacleInWay(Box obstacle, Point robotPos)
 {
-	actPoint.Mac(heading, DIST_MIN);
-}
-
-/******************************************************************************/
-void BugAlgorithm::wallFollowingToPoint(Point p, bool dir, Box box)
-{
-	double distanceMargin = 0.005;
-
-	while (distanceEuclid(actPoint, p) > distanceMargin)
-	{
-		wallFollowing(dir, box);
-	}
-
-	cout << "at leave point \n";
-	move();
-}
-
-/******************************************************************************/
-void BugAlgorithm::motionToGoal()
-{
-	actPoint.Mac(heading, DIST_MIN);
-	//cout << "motion to goal\n";
-}
-
-/**********************************************************************************************/
-void BugAlgorithm::halt()
-{
-}
-
-//----- Obstacle Avoidance ----//
-
-/**********************************************************************************************/
-bool BugAlgorithm::completeCycleAroundObstacle(Point robotPos, Point hitPoint)
-{
-	//.....
-	return true;
-}
-
-/**********************************************************************************************/
-void BugAlgorithm::identifyLeavePoint(bool direction, Point robotPos, Point goalPos, Box box)
-{
-	recordHitPoint(robotPos);
-	//wallFollowing(direction, box);
-	//double tot = totalDistance + 1;
-	//setTotalDistance(tot);
-	findLeavePoint(robotPos, getLastHitPoint());
+	return obstacle.isPointInsideAABB(robotPos);
 }
 
 /**********************************************************************************************/
 bool BugAlgorithm::goalReached(Point robotPos, Point goalPos, double distError)
 {
 	return (distanceEuclid(robotPos, goalPos) <= distError);
-}
-
-/**********************************************************************************************/
-//get the first hit point
-Point BugAlgorithm::getFirstHitPoint()
-{
-	vector<Point>::iterator it = hitPoint.begin();
-	return Point(*it);
-}
-
-/**********************************************************************************************/
-//get the last hit point
-Point BugAlgorithm::getLastHitPoint()
-{
-	vector<Point>::iterator it = hitPoint.end() - 1;
-	return Point(*it);
-}
-
-/**********************************************************************************************/
-//get the first leave point
-Point BugAlgorithm::getFirstLeavePoint()
-{
-	vector<Point>::iterator it = leavePoint.begin();
-	return ::Point(*it);
-}
-
-/**********************************************************************************************/
-//get the last leave point
-Point BugAlgorithm::getLastLeavePoint()
-{
-	vector<Point>::iterator it = leavePoint.end() - 1;
-	return Point(*it);
-}
-
-/**********************************************************************************************/
-void BugAlgorithm::recordHitPoint(Point point)
-{
-	if (!hitDefined)
-	{
-		// Add the hit point
-		hitPoint.push_back(point);
-		// now the robot can switch to the boundary following mode
-		hitDefined = true;
-		//isAlreadyLeftLastHitPoint = false;
-	}
-}
-
-/**********************************************************************************************/
-void BugAlgorithm::recordLeavePoint(Point point)
-{
-	leavePoint.push_back(point);
-}
-
-/**********************************************************************************************/
-bool BugAlgorithm::obstacleInFrontOfTheRobot()
-{
-	return(true);
-}
-
-/**********************************************************************************************/
-bool BugAlgorithm::goalIsReachable(Point robotPos, Point goalPos, double distError)
-{
-	bool res = false;
-	//if (obstacleInFrontOfTheRobot() && goalReached(robotPos, goalPos, distError))
-	if (goalReached(robotPos, goalPos, distError))
-		res = true;
-	//else if (!obstacleInFrontOfTheRobot())
-	//  res = true;
-	//cout << "Goal reachable from this point?? " << res << "\n";
-	return res;
 }
 
 /****************************************************************************************************
@@ -410,63 +204,4 @@ bool BugAlgorithm::IntersectionLineLine(Point p1, Point p2, Point p3, Point p4, 
 	par_t = intersection->SquareDistance(p3) / p4.SquareDistance(p3);
 	*t2 = sqrt(par_t);
 	return true;
-}
-
-/**********************************************************************************************
- * Find the points of intersection of a circle and a line segment. There max. 2 intersection points
- * returns number of intersection points
- */
-int BugAlgorithm::FindLineCircleIntersections(Point center, double radius, Point point1, Point point2, Point *intersection1, Point *intersection2)
-{
-	int ret = 0; // number of intersection points in the interval [0,1]
-	double dx, dy, A, B, C, det, t;
-	double cx = center.x;
-	double cy = center.y;
-	dx = point2.x - point1.x;
-	dy = point2.y - point1.y;
-
-	A = dx * dx + dy * dy;
-	B = 2 * (dx * (point1.x - cx) + dy * (point1.y - cy));
-	C = (point1.x - cx) * (point1.x - cx) + (point1.y - cy) * (point1.y - cy) - radius * radius;
-
-	det = B * B - 4 * A * C;
-	if ((A <= 0.0000001) || (det < 0))
-	{
-		// No real solutions.
-		return ret;
-	}
-	else if (det == 0)
-	{
-		// possibly one solution.
-		t = -B / (2 * A);
-		if (t >= 0.0 && t <= 1.0)
-		{
-			intersection1 = new Point(point1.x + t * dx, point1.y + t * dy, 0.0);
-			ret++;
-		}
-		return ret;
-	}
-	else
-	{
-		// possibly two solutions.
-		t = (double)((-B + sqrt(det)) / (2. * A));
-		if (t >= 0.0 && t <= 1.0)
-		{
-			intersection1 = new Point(point1.x + t * dx, point1.y + t * dy, 0.0);
-			ret++;
-		}
-		t = (double)((-B - sqrt(det)) / (2. * A));
-		if (t >= 0.0 && t <= 1.0)
-		{
-			intersection2 = new Point(point1.x + t * dx, point1.y + t * dy, 0.0);
-			ret++;
-		}
-		if (ret == 2)
-		{
-			ret--;
-			intersection1->Lerp(*intersection1, *intersection2, 0.5);
-		}
-
-		return ret;
-	}
 }
