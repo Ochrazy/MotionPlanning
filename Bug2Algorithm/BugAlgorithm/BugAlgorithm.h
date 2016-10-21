ee06f6a8e3fbb76c4823eafba63417f30a06c305
Johannes Wambach <johannes.wambach@gmail.com>
2016-10-21 17:53:57 +0200

    Add files via upload

--- Bug2Algorithm/BugAlgorithm/BugAlgorithm.h
+++ Bug2Algorithm/BugAlgorithm/BugAlgorithm.h
@@ -16,37 +16,28 @@ static const double DIST_ERROR = DIST_MIN / 2.; //minimum allowed distance betwe
 
 class BugAlgorithm
 {
-public:
-    enum { CLOCKWISE = true, COUNTERCLOCKWISE = false };
-
 protected:
-    bool firstMove; //used to avoid multiple definitions of hit points around and obstacle
-    bool direction, leftCheck, rightCheck, hitDefined, firstHit;
     double totalDistance;
     Point goalPosition;     //the goal position
     Point startPosition;    //start point position
     Point heading;          // direction to move
-    Point intermediate;     // intermediate Point
-    vector<Point> hitPoint;
-    vector<Point> leavePoint;
-    Point oldPoint;
+	Point intermediate;     // intermediate Point
     Point actPoint;
 	bool wallFollowingMode;
 	bool bWallFollowingClockwise;
 	Point cornerPoint;
 	Point latestHitPoint;
+	double dist_current;
+	int obstacleHit;
 
 public:
     BugAlgorithm::BugAlgorithm(const std::string& name);
 
     // ------ Virtuelle Methoden ----- //
-    virtual void goToLeavePoint(Point) {}
-    virtual bool researchComplete(Point, Point, Point) { return false; }
-    virtual bool leavePointFound() { return false; }
-    virtual void findLeavePoint(Point, Point) {}
-    virtual void wallFollowing(bool, Box) {};
+    virtual void wallFollowing(Point, Box) {};
+	virtual void findHeadingAlongWall(Point, Box) {};
 
-    void setIntermediatePoint(Point pt);
+	void setIntermediatePoint(Point pt);
     void setHeading(Point dir);
     void setHeading(double x, double y, double z = 0.f);
     void setGoalPosition(double x, double y);
@@ -60,28 +51,10 @@ public:
     // euklidischer Abstand
     double distanceEuclid(Point positionA, Point positionB);
 
-    void setTotalDistance(double value);
-    double getTotalDistance();
-
     bool update(Box obstacle[], Box robot[], int nObst);
-    int obstacleInWay(Box obstacle[], Box robot[], Point robotPos, int nObst);
-    void move();
-    void wallFollowingToPoint(Point p, bool dir, Box box);
-    void motionToGoal();
-    void halt();
-
-    //----- Obstacle Avoidance ----//
-    bool completeCycleAroundObstacle(Point robotPos, Point hitPoint);
-    void identifyLeavePoint(bool direction, Point robotPos, Point goalPos, Box box);
-    bool goalReached(Point robotPos, Point goalPos, double distError);
-    Point getFirstHitPoint();
-    Point getLastHitPoint();
-    Point getFirstLeavePoint();
-    Point getLastLeavePoint();
-    void recordHitPoint(Point point);
-    void recordLeavePoint(Point point);
-    bool obstacleInFrontOfTheRobot();
-    bool goalIsReachable(Point robotPos, Point goalPos, double distError);
+	int obstaclesInWay(Box obstacle[], Point robotPos, int nObst);
+	bool obstacleInWay(Box obstacle, Point robotPos);
+	bool goalReached(Point robotPos, Point goalPos, double distError);
 
     /****************************************************************************************************
      * Return value: true, if line from point p1, p2 intersects line from point p3, p4
@@ -92,12 +65,6 @@ public:
      * t2 the parameter value between [0,1]: intersection = p3 + t*(p4-p3)
      */
     bool IntersectionLineLine(Point p1, Point p2, Point p3, Point p4, Point *intersection, double *t1, double *t2);
-
-    /****************************************************************************************************
-     * Find the points of intersection of a circle and a line segment. There max. 2 intersection points
-     * returns number of intersection points
-     */
-    int FindLineCircleIntersections(Point center, double radius, Point point1, Point point2, Point *intersection1, Point *intersection2);
 };
 
 #endif /* __BUGALGORITHM_H__ */
