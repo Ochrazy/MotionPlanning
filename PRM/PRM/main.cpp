f4460e5f8957867e8412a50d9ced2a97f6fec3fb
Ochrazy <johannes.wambach@gmail.com>
2016-12-01 18:36:16 +0100

    basic PRM works

--- PRM/PRM/main.cpp
+++ PRM/PRM/main.cpp
@@ -6,6 +6,28 @@ using namespace std;
 namespace bg = boost::geometry;
 namespace bgi = boost::geometry::index;
 
+// test connection
+bool testConnection(Eigen::VectorXd firstNode, Eigen::VectorXd secondNode, WormCell& cell, float stepsize = 0.025f)
+{
+	Eigen::VectorXd segment(secondNode - firstNode), delta(5);
+	delta = segment.normalized() * stepsize; // stepsize .025
+	int steps = int(segment.norm() / stepsize);
+	Eigen::VectorXd tmpNode = firstNode;
+
+	do
+	{
+		if (!cell.CheckPosition(tmpNode))
+		{
+			return false;
+		}
+		else
+		{
+			tmpNode += delta;
+		}
+	} while (--steps > 0);
+
+	return true;
+}
 /***********************************************************************************************************************************/
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -19,11 +41,14 @@ int _tmain(int argc, _TCHAR* argv[])
 #ifdef TEST_CASE
 #if TEST_CASE == 0
 	// Example
-	cout << "Example" << endl;
-	 qStart << .0, -.2, DEG2RAD(0.), DEG2RAD(0.), DEG2RAD(0.);
-	 qGoal << .6, .6, DEG2RAD(90.f), DEG2RAD(0.f), DEG2RAD(0.f);
+	//cout << "Example" << endl;
+	qStart << .0, -.2, DEG2RAD(0.), DEG2RAD(0.), DEG2RAD(0.);
+	qGoal << .6, .6, DEG2RAD(90.f), DEG2RAD(0.f), DEG2RAD(0.f);
 
-	 /*
+	/*
+	//qStart << 0.986542f, 0.92289f, DEG2RAD(132.256f), DEG2RAD(-11.9016f), DEG2RAD (-107.666f);
+	//qGoal << 0.788461f, 0.56383f, DEG2RAD(141.03f), DEG2RAD(-4.15792f), DEG2RAD(-116.924f);
+	 
 	Eigen::VectorXd segment(qGoal - qStart), delta(5);
 	delta = segment.normalized() * stepsize;
 	int steps = int(segment.norm() / stepsize);
@@ -32,10 +57,10 @@ int _tmain(int argc, _TCHAR* argv[])
 	{
 		if (!cell.CheckPosition(qStart))
 		{
-			for (int i = 0; i < 10; ++i)
+			for (int i = 0; i < 100; ++i)
 			{
 				path.push_back(qStart);
-				qStart += delta * .1f;
+				qStart += delta * .01f;
 			}
 		}
 		else
@@ -93,13 +118,22 @@ int _tmain(int argc, _TCHAR* argv[])
 	qGoal << .6, .95, DEG2RAD(-90.f), 0., 0.;
 #endif
 #endif
-
+	
 	const int nNodes = 25000;
 	graph_t g(nNodes+2);
 
+	/*Eigen::VectorXd v(5);
+	Eigen::VectorXd v2(5);
+
+	v << 0.986542f, 0.92289f, DEG2RAD(132.256f), DEG2RAD(-11.9016f), DEG2RAD(-107.666f);
+	v2 << 0.788461f, 0.56383f, DEG2RAD(141.03f), DEG2RAD(-4.15792f), DEG2RAD(-116.924f);
+
+	bool what = testConnection( v, v2, cell);*/
+
+	
 	// 1. step: building up a graph g consisting of nNodes vertices
 	cout << "1. Step: building " << nNodes << " nodes for the graph" << endl;
-
+	
 	std::default_random_engine generator;
 	std::normal_distribution<double> distribution(.5, .25);
 
@@ -112,34 +146,13 @@ int _tmain(int argc, _TCHAR* argv[])
     // 2. step: building edges for the graph, if the connection of 2 nodes are in free space
     cout << "2. Step: buildung edges for the graph" << endl;
 	std::vector<std::pair<MyWorm, int>> result;
-	MyWorm currentWorm;
 	
 	for (int i = 0; i < nNodes; ++i) {
-		currentWorm = MyWorm(g[i].q_);
-		rtree.query(boost::geometry::index::nearest(currentWorm, 5), std::back_inserter(result));
+		rtree.query(boost::geometry::index::nearest(MyWorm(g[i].q_), 5), std::back_inserter(result));
 		for each (std::pair<MyWorm, int> worm in result)
 		{
-			// test connection
-			currentWorm = MyWorm(g[i].q_);
-			Eigen::VectorXd segment(worm.first.q() - currentWorm.q()), delta(5);
-			delta = segment.normalized() * stepsize;
-			int steps = int(segment.norm() / stepsize);
-
-			bool bAddEdge = true;
-			do
-			{
-				if (!cell.CheckPosition(currentWorm.q()))
-				{
-					bAddEdge = false;
-					break;
-				}
-				else
-				{
-					currentWorm.q() += delta;
-				}
-			} while (--steps > 0);
-
-			if (bAddEdge == true) boost::add_edge(i, worm.second, g);
+			if(testConnection(worm.first.q(), g[i].q_, cell))
+				boost::add_edge(i, worm.second, g);
 		}
 		result.clear();
 	}
@@ -149,68 +162,26 @@ int _tmain(int argc, _TCHAR* argv[])
 	//rtree.insert(make_pair(MyWorm(qStart), nNodes));
 	result.clear();
 	g[nNodes].q_ = qStart;
-	currentWorm = MyWorm(qStart);
-		rtree.query(boost::geometry::index::nearest(currentWorm, 5), std::back_inserter(result));
-		for each (std::pair<MyWorm, int> worm in result)
+	rtree.query(boost::geometry::index::nearest(MyWorm(qStart), 5), std::back_inserter(result));
+	for each (std::pair<MyWorm, int> worm in result)
+	{
+		if (testConnection(worm.first.q(), qStart, cell))
 		{
-			// test connection
-			currentWorm = MyWorm(qStart);
-			Eigen::VectorXd segment(worm.first.q() - currentWorm.q()), delta(5);
-			delta = segment.normalized() * stepsize;
-			int steps = int(segment.norm() / stepsize);
-
-			bool bAddEdge = true;
-			do
-			{
-				if (!cell.CheckPosition(currentWorm.q()))
-				{
-					bAddEdge = false;
-					break;
-				}
-				else
-				{
-					currentWorm.q() += delta;
-				}
-			} while (--steps > 0);
-
-			if (bAddEdge == true)
-			{
-				boost::add_edge(nNodes, worm.second, g);
-				break;
-			}
+			boost::add_edge(nNodes, worm.second, g);
+			break;
 		}
-		result.clear();
+	}
+	result.clear();
 
     // 4. Step: connecting goal configuration to graph
     cout << "4. Step: connecting goal configuration to graph" << endl;
 	//rtree.insert(make_pair(MyWorm(qGoal), nNodes + 1));
 	result.clear();
 	g[nNodes + 1].q_ = qGoal;
-	currentWorm = MyWorm(qGoal);
-	rtree.query(boost::geometry::index::nearest(currentWorm, 5), std::back_inserter(result));
+	rtree.query(boost::geometry::index::nearest(MyWorm(qGoal), 5), std::back_inserter(result));
 	for each (std::pair<MyWorm, int> worm in result)
 	{
-		// test connection
-		currentWorm = MyWorm(qGoal);
-		Eigen::VectorXd segment(worm.first.q() - currentWorm.q()), delta(5);
-		delta = segment.normalized() * stepsize;
-		int steps = int(segment.norm() / stepsize);
-
-		bool bAddEdge = true;
-		do
-		{
-			if (!cell.CheckPosition(currentWorm.q()))
-			{
-				bAddEdge = false;
-				break;
-			}
-			else
-			{
-				currentWorm.q() += delta;
-			}
-		} while (--steps > 0);
-
-		if (bAddEdge == true) 			
+		if (testConnection(worm.first.q(), qGoal, cell))
 		{
 			boost::add_edge(nNodes + 1, worm.second, g);
 			break;
@@ -228,16 +199,35 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	// Write the path: reverse search through predecessor map
 	int currentVertex = nNodes + 1; // end point
-	while ((currentVertex != nNodes) && (path.size() < 1000)) // start point
+	while ((currentVertex != nNodes)) // start point
 	{
 		path.push_back(g[currentVertex].q_);
 		currentVertex = p[currentVertex];
+
+		if (path.size() > 10000)
+			return EXIT_FAILURE;
 	}
 	path.push_back(g[currentVertex].q_);
 
+	// Path Refinement
+	// Go through path
+	reverse(path.begin(), path.end());
+	for (unsigned int current = 0; current < path.size(); current++)
+	{
+		// Find last successor
+		int lastSuccessor = current + 1;
+		for (unsigned int successor = current + 2; successor < path.size(); successor++)
+		{
+			if (testConnection(path[current], path[successor], cell))
+				lastSuccessor = successor;
+		}
+		if (lastSuccessor != current + 1)
+			path.erase(path.begin() + current + 1, path.begin() + lastSuccessor);
+	}
+	reverse(path.begin(), path.end());
+	
 	write_easyrob_program_file(path, "prm.prg");
-
 	write_gnuplot_file(g, "VisibilityGraph.dat");
-
+	
     return EXIT_SUCCESS;
 }
